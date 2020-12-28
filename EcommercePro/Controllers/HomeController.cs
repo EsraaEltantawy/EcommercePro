811ee2feb1ef5a2b31508531419d8e5e5031f071
esraab.eltantawy <esraab.eltantawy@gmail.com>
2020-12-28 12:46:08 +0200

    2

--- EcommercePro/Controllers/HomeController.cs
+++ EcommercePro/Controllers/HomeController.cs
@@ -9,8 +9,7 @@ namespace EcommercePro.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            ViewBag.Message = "Your application description page.";
+        { 
             return View();
         }
 

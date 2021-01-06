7834f37b4928afa92d5dfa46433494f637bd31f5
esraab.eltantawy <esraab.eltantawy@gmail.com>
2021-01-06 19:52:02 +0200

    Product Add

--- EcommercePro/Controllers/AdminController.cs
+++ EcommercePro/Controllers/AdminController.cs
@@ -42,8 +42,32 @@ namespace EcommercePro.Controllers
         }
         public ActionResult product() 
         {
-            return View();
+            return View(_unitOfWork.GetRepositoryInstance<Tbl_Product>().GetProduct());
         }
 
+
+
+        public ActionResult ProductEdit(int productId)
+        {
+           //ViewBag.CategoryList = GetCategory();
+            return View(_unitOfWork.GetRepositoryInstance<Tbl_Product>().GetFirstorDefault(productId));
+        }
+
+        [HttpPost]
+        public ActionResult ProductEdit(Tbl_Product tbl, HttpPostedFileBase file)
+        {
+            //string pic = null;
+            //if (file != null)
+            //{
+            //    pic = System.IO.Path.GetFileName(file.FileName);
+            //    string path = System.IO.Path.Combine(Server.MapPath("~/ProductImg/"), pic);
+            //    // file is uploaded
+            //    file.SaveAs(path);
+            //}
+            //tbl.ProductImage = file != null ? pic : tbl.ProductImage;
+            //tbl.ModifiedDate = DateTime.Now;
+            _unitOfWork.GetRepositoryInstance<Tbl_Product>().Update(tbl);
+            return RedirectToAction("Product");
+        }
     }
 }
